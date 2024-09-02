92b055023a12406257030d7435a37e4bba42ad29
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2024-09-03 05:49:10 +0900

    Update TankRobot_ServoMotor_ex1.ino

--- TankRobot_ServoMotor_ex1.ino
+++ TankRobot_ServoMotor_ex1.ino
@@ -1,5 +1,5 @@
 /*
-  YAHBOOM G1 Tank Robot (ver. Arduino) : LED example Code
+  YAHBOOM G1 Tank Robot (ver. Arduino) : Servo motor example Code
   - File Name : TankRobot_ServoMotor_ex1.ino
   - Update : 2024.09.03 (by yangjipsa)
 */ 
